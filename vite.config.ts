bcaf4c6e84c23c0a1f5ee4d18d2822c2da293262
Akhila Ohmkumar <akhilaohmkumar@gmail.com>
2025-09-26 20:09:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Memory-Game-/",   // 👈 use your exact repo name (case-sensitive!)
+  base: "/Memory-Game-/",   // repo name
   build: {
-    outDir: "docs",         // 👈 build into docs folder
+    outDir: "dist",         // default build folder
   },
 });
+
